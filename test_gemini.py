8e614dc0025227f91592a0e5dd49eb6b5d556c45
Buriburi-zaem0n <6uriburizaem0n@gmail.com>
2025-11-25 19:46:11 +0530

    Delete test_gemini.py

--- test_gemini.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-print("google-generativeai version:", genai.__version__)
-
-genai.configure(api_key="AIzaSyDNQF3-kP9WZOd6kxs6WJ9fNP_GpXX6pik")
-
-print("Available models:")
-for m in genai.list_models():
-    print("-", m.name)
